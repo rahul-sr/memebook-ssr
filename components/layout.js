5e076fa41a9c6183cd0890e64edf5035fa272555
rahulsrivastava <vnsrahulsri@gmail.com>
2020-05-03 14:27:05 +0530

    code

--- components/layout.js
+++ components/layout.js
@@ -16,8 +16,25 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="dns-prefetch" href="https://meme-api.herokuapp.com"/>
         <title>Memebook</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel='manifest' href='/manifest.json'></link>
+        <script>
+          // This is the "Offline page" service worker
+
+          // Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
+          // If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
+
+          /*
+          This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker,
+          tell the user when there is an update available and let the user know when your PWA is ready to use offline.
+          */
+
+          import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+          const el = document.createElement('pwa-update');
+          document.body.appendChild(el);
+        </script>
+        <script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <script data-ad-client="ca-pub-6223473093447406" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <nav className="navbar navbar-inverse">
